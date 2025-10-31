f9672c0f351264d726b96fe584206e16a683564c
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-10-31 10:11:10 -0400

    Changed file name. Added some basic information about Python variables.

---
+++ 03_VARIABLES/01_basics.py
@@ -0,0 +1,7 @@
+# Python is a dynamically TYPED language like PHP
+# Python variable names cannot start with a #
+# Python variable names cannot contain special characters other than _
+# Python variables are case-sensitive
+# Python variables can contain a-z, A-Z, #, and _
+
+
