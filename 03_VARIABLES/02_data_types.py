dae725859d8be9d90c2a85bcb8db615e3ffb3d89
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-10-31 10:39:17 -0400

    Adding information about data types

---
+++ 03_VARIABLES/02_data_types.py
@@ -0,0 +1,9 @@
+# Python supports several built-in data types:
+#   Integers (int) : Whole numbers (10, -5)
+#   Float (float)  : Decimal numbers (3.14, -0.001)
+#   String (str)   : Text data enclosed in quotes ("Hello", 'Python')
+#   Booleans (bool): Represents a True or False
+#   Lists          : Ordered, MUTABLE collections ([1, 2, 3])
+#   Tuples         : Ordered, IMMUTABLE collections ( (1, 2, 3))
+#   Sets           : Unordered collections of unique elements ({1, 2, 3})
+#   Dictionaries   : Key-value pairs this allows for mixed pairs ({"name": "Allice", "age": 25})
