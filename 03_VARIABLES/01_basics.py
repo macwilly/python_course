ceb66f6ad6758aae6d6e6b7b322a17a9631d32f2
Mackenzie Willard <mtwillard29@gmail.com>
2025-11-03 14:41:06 -0500

    Variables (#3)
    * Second lesson. Exploring Python Syntax.
    
    * New Variables lesson and creating the first file for it.
    
    * Changed file name. Added some basic information about Python variables.
    
    * Renamed the main.py to 10_basics
    
    * Examples of variables
    
    * Adding information about data types
    
    * Showing how to create basic variables, as well as print the type of the variable.
    
    ---------
    
    Co-authored-by: Mackenzie Willard <Mackenzie.Willard@truetandem.com>

---
+++ 03_VARIABLES/01_basics.py
@@ -0,0 +1,10 @@
+# Python is a dynamically TYPED language like PHP
+# Python variable names cannot start with a #
+# Python variable names cannot contain special characters other than _
+# Python variables are case-sensitive
+# Python variables can contain a-z, A-Z, #, and _
+
+my_string = "Hello World"
+my_int = 123
+my_float = 12.34
+my_bool = True
---
+++ 03_VARIABLES/02_data_types.py
@@ -0,0 +1,22 @@
+# Python supports several built-in data types:
+#   Integers (int) : Whole numbers (10, -5)
+#   Float (float)  : Decimal numbers (3.14, -0.001)
+#   String (str)   : Text data enclosed in quotes ("Hello", 'Python')
+#   Booleans (bool): Represents a True or False
+#   Lists          : Ordered, MUTABLE collections ([1, 2, 3])
+#   Tuples         : Ordered, IMMUTABLE collections ( (1, 2, 3))
+#   Sets           : Unordered collections of unique elements ({1, 2, 3})
+#   Dictionaries   : Key-value pairs this allows for mixed pairs ({"name": "Allice", "age": 25})
+
+
+age = 35
+
+name = "Mackenzie"
+
+favorite_pi = 3.14
+
+is_tall = True
+
+print(age)
+
+print(type(age))
