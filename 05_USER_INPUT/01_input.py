4c24d2eb5d37c9b51ccc1874330757b561b5c0d7
Mackenzie Willard <mtwillard29@gmail.com>
2025-11-03 19:59:24 -0500

    User input (#5)
    * Initial user input file
    
    * Showing how the input function takes user input from the console.
    
    * Creating a simple add function from user input. Also playing with string/print concat
    
    ---------
    
    Co-authored-by: Mackenzie Willard <Mackenzie.Willard@truetandem.com>

---
+++ 05_USER_INPUT/01_input.py
@@ -0,0 +1,6 @@
+# Use the input(string $prompt) to take input from a user
+user_input_a = input("Enter a number: ")
+print(user_input_a)
+
+user_input_b = input("Enter your name: ")
+print(user_input_b)
