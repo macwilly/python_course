7f774dd119946c7f5f9d64151f9b3a47fda34384
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-11-04 13:19:32 -0500

    Basic math operators that follow PEMDAS. Division is cool with returning a float or there is floor division

---
+++ 07_OPERATORS/01_operators.py
@@ -0,0 +1,4 @@
+# Most of the operators are what you would expect. There is floor division // which will return an int value
+# Division will return a float even when it is a whole number.
+print(10/3)
+print(10//3)
