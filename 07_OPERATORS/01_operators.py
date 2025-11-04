90d2757ef8d95c010ae4ac00be1c27a38e61071f
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-11-04 17:44:27 -0500

    Looking at conditional operators. Strings are compared with ==

--- 07_OPERATORS/01_operators.py
+++ 07_OPERATORS/01_operators.py
@@ -2,3 +2,14 @@
 # Division will return a float even when it is a whole number.
 print(10/3)
 print(10//3)
+
+
+# Conditional Operator
+# These will always return a true or false value
+
+string_a = "Hello"
+string_b = "World"
+if string_a == string_b:
+    print("String is equal to string")
+else:
+    print("String is not equal to string")
