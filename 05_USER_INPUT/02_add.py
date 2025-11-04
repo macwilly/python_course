50c188924c298c80315de061c66969a373273357
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-11-03 19:56:51 -0500

    Creating a simple add function from user input. Also playing with string/print concat

---
+++ 05_USER_INPUT/02_add.py
@@ -0,0 +1,8 @@
+first_number = int(input("Enter a number: "))
+second_number = int(input("Enter another number: "))
+
+# Printing the sum of the two numbers from the user.
+# String interpolation is similar to other languages such as C# note that the special character at the start is an 'f'
+# Also when concatinating with , the print function will add a space to the end of the first section.
+print(f"The sum of {first_number} and {second_number} is", first_number + second_number)
+print(first_number + second_number , "World")
