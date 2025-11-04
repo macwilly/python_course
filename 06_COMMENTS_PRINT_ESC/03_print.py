ebc66727f9141b2e865359bfb4812ceabd75dbef
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-11-03 20:45:57 -0500

    Looking at print() function features. Explored these in User input as well.

---
+++ 06_COMMENTS_PRINT_ESC/03_print.py
@@ -0,0 +1,4 @@
+print("Hello World")
+# print() function will take multiple arguments and add a space between them.
+# print() also will always add a new line to the end of the statement.
+print("Hello World", "Mac")
