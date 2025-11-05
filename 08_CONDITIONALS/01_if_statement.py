638a987adb6860ca3c7083cceb60ace3068c4d74
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-11-05 09:03:52 -0500

    Basic if statement

---
+++ 08_CONDITIONALS/01_if_statement.py
@@ -0,0 +1,9 @@
+# Should be pretty understandable if statements
+# else if, is shortened to elif
+age = int(input("Enter your age: "))
+
+if age >= 18:
+    print("You are old enough to drive.")
+
+
+print("End of Program.")
