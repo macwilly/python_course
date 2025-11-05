5d1e972c1f8ba1c805ebb847531b37c82c57d05b
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-11-04 20:22:32 -0500

    Sixth question.

---
+++ PRACTICE_SET_ONE/06_q6.py
@@ -0,0 +1,7 @@
+first_number = int(input("Please enter the first number: "))
+second_number = int(input("Please enter the second number: "))
+
+print(f"The sum of {first_number} + {second_number} =", first_number + second_number)
+print(f"The difference of {first_number} - {second_number} =", first_number - second_number)
+print(f"The product of {first_number} * {second_number} =", first_number * second_number)
+print(f"The quotient of {first_number} / {second_number} =", first_number / second_number)
