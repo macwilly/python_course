145c7772662187cc59129d05c71fedd913a32cff
Mackenzie Willard <mtwillard29@gmail.com>
2025-11-05 09:11:45 -0500

    Conditionals (#9)
    * Basic if statement
    
    * If else statement
    
    * Basic if elif else ladder
    
    ---------
    
    Co-authored-by: Mackenzie Willard <Mackenzie.Willard@truetandem.com>

---
+++ 08_CONDITIONALS/03_if_elif_else.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age:\n"))
+
+if age > 18:
+    print("You are old enough to drive.")
+elif age == 18:
+    print("Lets schedule an interview.")
+else:
+    print("You are old enough to drive.")
+
+print("End of Program.")
