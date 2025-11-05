26d6addf36ef1eb9a2c2dee88eb877d14bbcf944
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-11-04 20:29:50 -0500

    Eigth question.

---
+++ PRACTICE_SET_ONE/08_q8.py
@@ -0,0 +1,6 @@
+user_number = int(input("Please enter a number: "))
+
+square = user_number ** 2
+cube = user_number ** 3
+print(f"The square of {user_number} is {square}.")
+print(f"The cube of {user_number} is {cube}.")
