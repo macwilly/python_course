015f8fb5fb6b23245fa0b205be83e736d1d82888
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-11-11 10:45:04 -0500

    Going over some basic slicing

---
+++ 11_STRINGS/03_slicing.py
@@ -0,0 +1,10 @@
+# Slice Syntax Recap string[start:stop:step]
+# In Slicing the first argument is the index to start at. the second argument is the index -1 to stop at
+# the default of the first argument is 0
+# With a -1 as the second argument it will return from the given start index to the end of the string.
+# Doing ::-1 will print the whole string in reverse
+name = "MackenzieWillard"
+
+print(name[3:5])
+print(name[3:-1])
+print(name[::-1])
