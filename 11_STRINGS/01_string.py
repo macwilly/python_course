5858f09406720c1e730427004c1c0cc5025997b0
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-11-11 10:12:34 -0500

    Showing the difference between ' " '''

---
+++ 11_STRINGS/01_string.py
@@ -0,0 +1,8 @@
+name       = "Harry"
+name_two   = 'Harry'
+# The Triple ''' """ quote can be used for multi lined strings
+name_three = '''Harry'''
+
+print(name)
+print(name_two)
+print(name_three)
