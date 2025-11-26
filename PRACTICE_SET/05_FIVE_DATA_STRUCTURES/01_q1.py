de1fda70e76aee60a2435c71e1222808c14a6c23
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-11-25 20:08:45 -0500

    First few questions

---
+++ PRACTICE_SET/05_FIVE_DATA_STRUCTURES/01_q1.py
@@ -0,0 +1,5 @@
+fruit_list = ["apple", "banana", "cherry"]
+print(fruit_list[0])
+fruit_list[1] = "orange"
+print(fruit_list)
+print(len(fruit_list))
---
+++ PRACTICE_SET/05_FIVE_DATA_STRUCTURES/01_q2.py
@@ -0,0 +1,6 @@
+num_list = []
+
+for i in range(1,11):
+    num_list.append(i)
+print(num_list[0:3])
+print(num_list[-3:])
