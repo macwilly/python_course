44bf574148afcd75bcee3fa975e91f360d13d18f
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-12-02 18:00:46 -0500

    Going over magic or double underscore methods for a class

---
+++ 20_ADVANCED_CONCEPTS/05_magic_dunder_methods.py
@@ -0,0 +1,27 @@
+# dunder = duble under score __
+class Employee:
+    company = "HP"
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+    # more for the user
+    def __str__(self):
+        return f"{self.name} {self.salary}"
+
+    # repr is mostly used for debuffing
+    def __repr__(self):
+        return f"name = {self.name}\nsalary {self.salary}"
+
+    # The developer will decide how to assign what len will return
+    def __len__(self):
+        return len(self.name)
+
+
+e = Employee('Tiger Woods', 20000)
+
+print(e.name, e.salary)
+print(str(e))
+print(repr(e))
+print(len(e))
