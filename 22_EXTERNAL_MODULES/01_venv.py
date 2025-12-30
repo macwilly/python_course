f8ed7eeeaf02ed3e9e9c8310ee265b50998ea191
Mackenzie Willard <Mackenzie.Willard@truetandem.com>
2025-12-30 14:26:28 -0500

    Creating virtual environments
    Two environments. Those are left out due to the gitignore

---
+++ 22_EXTERNAL_MODULES/01_venv.py
@@ -0,0 +1,18 @@
+# Virtual Environments
+# In command line
+# pip install virtualenv
+# python -m venv environment-name // this will create a new env directory of that name
+# python -m venv env1
+# to activate on Windows terminal or powershell
+#    ./env1/Scripts/Activate.ps1
+# to activate on Mac or Linux
+#    source ./env1/Scripts/activate
+
+
+# to make a requirments file do
+#   pip freeze > requirements.txt
+
+#to install from requirements.txt
+# pip install -r requirements.txt
+
+import moviepy
